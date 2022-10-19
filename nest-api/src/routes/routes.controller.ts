ce6a5a919f47a9b7b709e35ed054f8b00a5aad86
João Alves <alvezsjv@gmail.com>
2022-10-19 19:03:42 -0300

    feat(api): adds websocket

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -6,23 +6,18 @@ import {
   Patch,
   Param,
   Delete,
-  Inject,
-  OnModuleInit,
 } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
-import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
-import { Producer } from 'kafkajs';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { RoutesGateway } from './routes.gateway';
 
 @Controller('routes')
-export class RoutesController implements OnModuleInit {
-  private kafkaProducer: Producer;
-
+export class RoutesController {
   constructor(
     private readonly routesService: RoutesService,
-    @Inject('KAFKA_SERVICE')
-    private kafkaClient: ClientKafka,
+    private routeGateway: RoutesGateway,
   ) {}
 
   @Post()
@@ -50,23 +45,6 @@ export class RoutesController implements OnModuleInit {
     return this.routesService.remove(+id);
   }
 
-  async onModuleInit() {
-    this.kafkaProducer = await this.kafkaClient.connect();
-  }
-
-  @Get(':id/start')
-  startRoute(@Param('id') id: string) {
-    this.kafkaProducer.send({
-      topic: 'route.new-direction',
-      messages: [
-        {
-          key: 'route.new-direction',
-          value: JSON.stringify({ routeId: id, clientId: '' }),
-        },
-      ],
-    });
-  }
-
   @MessagePattern('route.new-position')
   consumeNewPosition(
     @Payload()
@@ -77,6 +55,6 @@ export class RoutesController implements OnModuleInit {
       finished: boolean;
     },
   ) {
-    console.log(message);
+    this.routeGateway.sendPosition(message);
   }
 }
